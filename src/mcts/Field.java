14371172d94ab65bb244b475a90d1df1facde742
bleonajashari <93947087+bleonajashari@users.noreply.github.com>
2023-05-17 16:31:32 +0200

    Add Field class implementation

---
+++ src/mcts/Field.java
@@ -0,0 +1,14 @@
+package mcts;
+
+/**
+ * Class representing a Field in a matrix
+ */
+public class Field {
+  public int row;
+  public int col;
+
+  public Field(int inRow, int inCol) {
+    row = inRow;
+    col = inCol;
+  }
+}
