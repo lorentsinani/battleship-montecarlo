f0ae8b3b81a7a51e9df288487058875ddc6d6c78
bleonajashari <93947087+bleonajashari@users.noreply.github.com>
2023-05-17 16:54:22 +0200

    Add playNextMove method

--- src/mcts/Node.java
+++ src/mcts/Node.java
@@ -1,6 +1,10 @@
 package mcts;
 
 import battleship.Player;
+import battleship.Randomizer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class Node {
   private Node parent; // null means root node
@@ -15,6 +19,8 @@ class Node {
   private int plays = 0;
   private int wins = 0;
 
+  private List<Node> children;
+
   /**
    * Initialize a Node.
    *
@@ -26,6 +32,34 @@ class Node {
     this.self = self.deepClone();
     this.opponent = opponent.deepClone();
 
+    this.children = new ArrayList<>();
+  }
+
+  public void playNextMove(Field move) {
+    this.move = move;
+
+    if (opponent.playerGrid.hasShip(move.row, move.col)) {
+      self.oppGrid.markHit(move.row, move.col);
+      opponent.playerGrid.markHit(move.row, move.col);
+    } else {
+      self.oppGrid.markMiss(move.row, move.col);
+      opponent.playerGrid.markMiss(move.row, move.col);
+    }
+
+  }
+
+  public Node randomChild() {
+    if (children.isEmpty()) return null;
+
+    return children.get(Randomizer.nextInt(0, children.size() - 1));
+  }
+
+  public void addChild(Node child) {
+    this.children.add(child);
+  }
+
+  public List<Node> getChildren() {
+    return this.children;
   }
 
   public void incrementPlays() {
